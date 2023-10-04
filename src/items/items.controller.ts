58e13f326cfc4fccacb2f3682f07fc285d1b7e0f
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2023-10-05 00:42:21 +0530

    add the crud methods for items

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
 
 @Controller('items')
@@ -8,8 +8,23 @@ export class ItemsController {
         return 'get all items';
     }
 
+    @Get(":id")
+    findOne(@Param() param): string {
+        return `Item: ${param.id}`;
+    }
+
     @Post()
     create(@Body() createItemDto: CreateItemDto): string {
         return `Name: ${createItemDto.name} Description: ${createItemDto.description}`;
+    }
+
+    @Put(":id")
+    update(@Body() updateItemDto: CreateItemDto, @Param("id") id): string {
+        return `updated ${id} with the Name: ${updateItemDto.name}`
+    }
+
+    @Delete(":id")
+    delete(@Param("id") id) {
+        return `Deleted item with ID - ${id}`;
     }
 }
