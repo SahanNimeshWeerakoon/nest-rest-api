7266da50d1a4e2060572d373b215ce82b4280fa4
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2023-10-05 17:28:10 +0530

    end of tutorial

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,34 +1,43 @@
-import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
-import { CreateItemDto } from './dto/create-item.dto';
-import { ItemsService } from './items.service';
-import { Item } from './interfaces/item.interface';
-
-@Controller('items')
-export class ItemsController {
-    constructor(private readonly _itemsService: ItemsService) {}
-
+import {
+    Controller,
+    Get,
+    Post,
+    Put,
+    Delete,
+    Body,
+    Param,
+  } from '@nestjs/common';
+  import { CreateItemDto } from './dto/create-item.dto';
+  import { ItemsService } from './items.service';
+  import { Item } from './interfaces/item.interface';
+  
+  @Controller('items')
+  export class ItemsController {
+    constructor(private readonly itemsService: ItemsService) {}
+  
     @Get()
-    findAll(): Item[] {
-        return this._itemsService.findAll();
+    findAll(): Promise<Item[]> {
+      return this.itemsService.findAll();
     }
-
-    @Get(":id")
-    findOne(@Param() param): string {
-        return `Item: ${param.id}`;
+  
+    @Get(':id')
+    findOne(@Param('id') id): Promise<Item> {
+      return this.itemsService.findOne(id);
     }
-
+  
     @Post()
-    create(@Body() createItemDto: CreateItemDto): string {
-        return `Name: ${createItemDto.name} Description: ${createItemDto.description}`;
+    create(@Body() createItemDto: CreateItemDto): Promise<Item> {
+      return this.itemsService.create(createItemDto);
     }
-
-    @Put(":id")
-    update(@Body() updateItemDto: CreateItemDto, @Param("id") id): string {
-        return `updated ${id} with the Name: ${updateItemDto.name}`
+  
+    @Delete(':id')
+    delete(@Param('id') id): Promise<Item> {
+      return this.itemsService.delete(id);
     }
-
-    @Delete(":id")
-    delete(@Param("id") id) {
-        return `Deleted item with ID - ${id}`;
+  
+    @Put(':id')
+    update(@Body() updateItemDto: CreateItemDto, @Param('id') id): Promise<Item> {
+      return this.itemsService.update(id, updateItemDto);
     }
-}
+  }
+  
